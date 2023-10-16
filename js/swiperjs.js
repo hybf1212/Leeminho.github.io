3db7f8b2bd80dcbaaf2ae01155881be13f43f6a1
hybf1212 <hybf1212@naver.com>
2023-10-16 22:26:00 +0900

    projects 마무리

--- js/swiperjs.js
+++ js/swiperjs.js
@@ -1,13 +1,41 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper.swiper-1", {
     spaceBetween: 20,
     centeredSlides: true,
     
     pagination: {
-      el: ".swiper-pagination",
+      el: ".swiper-pagination.swiper-1",
       type: "fraction",
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next.swiper-1",
+      prevEl: ".swiper-button-prev.swiper-1",
     },
   });
+
+var swiper = new Swiper(".mySwiper.swiper-2", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  
+  pagination: {
+    el: ".swiper-pagination.swiper-2",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next.swiper-2",
+    prevEl: ".swiper-button-prev.swiper-2",
+  },
+});
+
+var swiper = new Swiper(".mySwiper.swiper-3", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  
+  pagination: {
+    el: ".swiper-pagination.swiper-3",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next.swiper-3",
+    prevEl: ".swiper-button-prev.swiper-3",
+  },
+});
